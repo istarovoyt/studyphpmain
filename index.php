a7bb9a4b7473148950048ab96e187115ba0d4c38
istarovoyt <starovoyt107@gmail.com>
2017-07-04 11:19:51 +0300

    добавлена запись strpos

--- index.php
+++ index.php
@@ -3,4 +3,15 @@
     $str = "Hello, my name is Igor Starovoyt!";
     $result = strlen($str);
     echo  $result;
+
+    echo "<br>";
+
+    //Функция strpos - возвращает позицию вхождения подстроки
+    $str_1 = "This is a fucking string, motherfucker!";
+    $res_1 = strpos($str_1, "fu");
+    echo $res_1;
+
+    echo "<br>";
+
+    //
 ?>
